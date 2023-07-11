bfdaf51460f812b2076736cb4b9a759d67282cb3
Crowdbotics <team@crowdbotics.com>
2023-07-11 01:26:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-feather-42081",
+  url: "https://royal-feather-42081.botics.co",
+  api: "https://royal-feather-42081.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
